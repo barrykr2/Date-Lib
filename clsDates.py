30f236b72dbad6703ec5f4e1694ae135ae02024a
Barry Kruyssen <533686+barrykr2@users.noreply.github.com>
2022-10-31 12:57:13 +1000

    Add files via upload
    Fixed bug in addDay function and added subtractDay function.
    Add timing to test cases

--- clsDates.py
+++ clsDates.py
@@ -11,6 +11,8 @@
 #       "ddmm" will substitute the day and month of the last date processed
 #       "d/m" will substitute the day and month of the last date processed
 #       "ddmmyy" will substitute the expand to a date
+# By default short cut keys is turned off so as to run less code when doing straight validation.
+# The short cut keys should only be needed when called from a GUI 
 #
 # Please see the test cases at the bottom of the code for examples
 #
@@ -44,7 +46,7 @@ class check_date:
         
     def setSelf(self, formatting = ""):
         formatting = self.setDefaultFormattedDate(formatting)
-            
+
         self.day = self.dateObj.day
         self.month = self.dateObj.month
         self.year = self.dateObj.year
@@ -57,6 +59,11 @@ class check_date:
             formatting = defaultFormattedDate[self.expectedInputFormat.value]
         return formatting
 
+    def replaceSeperators(self, date):
+        # replace all date seperators with "-" for uniformity
+        space_punct_dict = dict((ord(punct), '-') for punct in string.punctuation)
+        return date.translate(space_punct_dict)
+        
     def processShortCutKeys(self, date):
         # check if the date is using the prior date object to calc a new date
         if date == '.':                             # use prior date
@@ -69,8 +76,7 @@ class check_date:
             date = dateobj.strftime(defaultFullDate)
 
         # replace all date seperators with "-" for uniformity
-        space_punct_dict = dict((ord(punct), '-') for punct in string.punctuation)
-        tmpDate = date.translate(space_punct_dict)
+        tmpDate = self.replaceSeperators(date)
 
         # check if incomplete date, try to format and add year if required
         if len(tmpDate) <= 6:
@@ -89,38 +95,37 @@ class check_date:
                 tmpDate = str(tmpArray[0]) + '-' + str(tmpArray[1]) + '-' + str(self.priorObj.year)
         return tmpDate
 
-    def dateCheck(self, date, formatting = ''):
+    def dateCheck(self, date, formatting = ""):
         # Initialise date object
         self.dateObj = datetime.strptime(nullDate, defaultFullDate)
         
         if date:
+            if self.allowShortCutKeys:
+                tmpDate = self.processShortCutKeys(date)
+            else:
+                # replace all date seperators with "-" for uniformity
+                tmpDate = self.replaceSeperators(date)
+
+            dayFirst = False        # US format mm-dd-yyyy
+            if self.expectedInputFormat == EF.Day_Month_Year:
+                dayFirst = True     # Australian format dd-mm-yyyy
+
             try:
-                if self.allowShortCutKeys:
-                    tmpDate = self.processShortCutKeys(date)
-                else:
-                    # replace all date seperators with "-" for uniformity
-                    space_punct_dict = dict((ord(punct), '-') for punct in string.punctuation)
-                    tmpDate = date.translate(space_punct_dict)
-
-                dayFirst = False        # US format mm-dd-yyyy
-                if self.expectedInputFormat == EF.Day_Month_Year:
-                    dayFirst = True     # Australian format dd-mm-yyyy
-
-                # get date object
+                # get date object - The next line is where the actual date is validated                                                                                         is whe                                                         
                 self.dateObj = parse(tmpDate, dayfirst = dayFirst)
                 self.isValid = datetime.strftime(self.dateObj, defaultFullDate) != nullDate
-
-                # only allow dates upto 5 years in the future
-                if self.dateObj.year > datetime.today().year + 5:
-                    tmpYear = self.dateObj.year - 100
-                    self.dateObj = self.dateObj.replace(year = tmpYear)
-
-                # reset variables
-                self.setSelf(self.setDefaultFormattedDate(formatting))
             except Exception as e:
                 self.isValid = False
-                print(e)
-                
+                #print(e)                
+
+            # only allow dates upto 5 years in the future
+            if self.dateObj.year > datetime.today().year + 5:
+                tmpYear = self.dateObj.year - 100
+                self.dateObj = self.dateObj.replace(year = tmpYear)
+
+            # reset variables
+            self.setSelf(self.setDefaultFormattedDate(formatting))
+
         return self.formattedDate
     
     def formatDate(self, date = nullDate, formatting = ""):
@@ -135,49 +140,77 @@ class check_date:
             self.dateCheck(date, formatting)    # check date passed in
 
         dateobj = self.dateObj + timedelta(numberOfDays)
-        self.dateCheck(dateobj.strftime(defaultFullDate), formatting)
+        self.dateCheck(dateobj.strftime(self.setDefaultFormattedDate(formatting)), formatting)
         return self.formattedDate
 
+    def subtractDay(self, numberOfDays = 1, formatting = "", date = nullDate):
+        return self.addDay(numberOfDays * -1, formatting, date)
+
 if __name__ == "__main__":
-    def testCheck(testId, expectedResult, restultReturned, displayErrorsOnly = True):
+    def testCheck(testId, expectedResult, restultReturned, now, displayErrorsOnly = True):
         isError = ''
         if expectedResult != restultReturned:
             isError = 'Error: '
             
         if (not displayErrorsOnly) or len(isError) > 0:
-            print(isError + testId + ' - Expected "' + expectedResult + '": = ' + restultReturned)
+            actualNow = datetime.now()
+            deltaTime = (actualNow - now)
+            print(("%.6f" % deltaTime.total_seconds()) + ' ' + isError + testId + ' - Expected "' +
+                  expectedResult + '": = ' + restultReturned)
+
+        now = datetime.now() # time object
+
+        return now
             
-    # Test code
+    # Test cases
+    startTime = datetime.now() # time object
+    print('-------------------- Testing Started ' + str(startTime.time()) + ' --------------------')
     displayErrorsOnly = False
+    now = datetime.now() # time object
+    
     chk = check_date(nullDate)
-    testCheck('chk', 'False', str(chk.isValid), displayErrorsOnly)
+    now = testCheck('chk0 initialise class', 'nothing', 'nothing', now, displayErrorsOnly)
+    now = testCheck('chk1', 'False', str(chk.isValid), now, displayErrorsOnly)
 
     x = check_date('23-09-71')
-    testCheck('x0', 'True', str(x.isValid), displayErrorsOnly)
-    testCheck('x1', '1971-09-23', str(x.fullDate), displayErrorsOnly)
-    testCheck('x2', '23', str(x.day), displayErrorsOnly)
-    testCheck('x3', '23-Sep-1971', x.formattedDate)
-    testCheck('x4', 'Feb-01-1960', str(x.formatDate('01/02/1960', '%b-%d-%Y')), displayErrorsOnly) # US formatted
-    testCheck('x5', '1960-02-01', str(x.fullDate), displayErrorsOnly)      
-    testCheck('x6', '1963 Sep 23', str(x.formatDate('1963-Sep-23', '%Y %b %d')), displayErrorsOnly)
-    testCheck('x7', '1963-09-23', str(x.fullDate), displayErrorsOnly)
+    now = testCheck('get another instance of class', 'nothing', 'nothing', now, displayErrorsOnly)
+    now = testCheck('x0', 'True', str(x.isValid), now, displayErrorsOnly)
+    now = testCheck('x1', '1971-09-23', str(x.fullDate), now, displayErrorsOnly)
+    now = testCheck('x2', '23', str(x.day), now, displayErrorsOnly)
+    now = testCheck('x3', '23-Sep-1971', x.formattedDate, now, displayErrorsOnly)
+    now = testCheck('x4', 'Feb-01-1960', str(x.formatDate('01/02/1960', '%b-%d-%Y')), now, displayErrorsOnly) # US formatted
+    now = testCheck('x5', '1960-02-01', str(x.fullDate), now, displayErrorsOnly)      
+    now = testCheck('x6', '1963 Sep 23', str(x.formatDate('1963-Sep-23', '%Y %b %d')), now, displayErrorsOnly)
+    now = testCheck('x7', '1963-09-23', str(x.fullDate), now, displayErrorsOnly)
     x.expectedInputFormat = EF.Month_Day_Year
-    testCheck('x8', '2022-Mar-01', x.formatDate('03/01/22', '%Y-%b-%d'), displayErrorsOnly)
-    testCheck('x9', '2022-03-01', str(x.fullDate), displayErrorsOnly)      
-    testCheck('x10', '2022-Mar-01', str(x.formattedDate), displayErrorsOnly)      
-    testCheck('x11', 'Mar-02-2022', str(x.addDay(1,'%b-%d-%Y')), displayErrorsOnly)
-    testCheck('x12', 'Apr-01-2022', str(x.addDay(30)), displayErrorsOnly)
-    testCheck('x13', 'Apr-02-2022', str(x.addDay()), displayErrorsOnly)
-    testCheck('x14', 'Dec-17-2022', str(x.formatDate('17 Dec 2022')), displayErrorsOnly)
-    testCheck('x15', 'Jan-02-1960', x.dateCheck('01/02/1960'), displayErrorsOnly)
+    now = testCheck('x8', '2022-Mar-01', x.formatDate('03/01/22', '%Y-%b-%d'), now, displayErrorsOnly)
+    now = testCheck('x9', '2022-03-01', str(x.fullDate), now, displayErrorsOnly)      
+    now = testCheck('x10', '2022-Mar-01', str(x.formattedDate), now, displayErrorsOnly)      
+    now = testCheck('x11', 'Mar-02-2022', str(x.addDay(1,'%b-%d-%Y')), now, displayErrorsOnly)
+    now = testCheck('x12', 'Apr-01-2022', str(x.addDay(30)), now, displayErrorsOnly)
+    now = testCheck('x13', 'Apr-02-2022', str(x.addDay()), now, displayErrorsOnly)
+    now = testCheck('x14', 'Dec-17-2022', str(x.formatDate('17 Dec 2022')), now, displayErrorsOnly)
+    now = testCheck('x15', 'Jan-02-1960', x.dateCheck('01.02,1960'), now, displayErrorsOnly)
     x.allowShortCutKeys = True
-    testCheck('x16', 'Jan-02-1960', x.dateCheck('.'), displayErrorsOnly)
-    testCheck('x17', 'Jan-03-1960', x.dateCheck('..'), displayErrorsOnly)
-    testCheck('x18', 'Jan-02-1960', x.dateCheck('--'), displayErrorsOnly)
-    testCheck('x19', 'Sep-23-1963', x.dateCheck('092363'), displayErrorsOnly)
-    testCheck('x20', 'Sep-23-1963', x.dateCheck('09/23'), displayErrorsOnly)
-    testCheck('x21', 'Sep-23-1963', x.dateCheck('0923'), displayErrorsOnly)
-    testCheck('x22', 'Apr-05-1963', x.dateCheck('4/5'), displayErrorsOnly)
-    testCheck('x23', 'Apr-11-1963', x.dateCheck('11'), displayErrorsOnly)
+    now = testCheck('x16', 'Jan-02-1960', x.dateCheck('.'), now, displayErrorsOnly)
+    now = testCheck('x17', 'Jan-03-1960', x.dateCheck('..'), now, displayErrorsOnly)
+    now = testCheck('x18', 'Jan-02-1960', x.dateCheck('--'), now, displayErrorsOnly)
+    now = testCheck('x19', 'Sep-23-1963', x.dateCheck('092363'), now, displayErrorsOnly)
+    now = testCheck('x20', 'Sep-23-1963', x.dateCheck('09/23'), now, displayErrorsOnly)
+    now = testCheck('x21', 'Sep-23-1963', x.dateCheck('0923'), now, displayErrorsOnly)
+    now = testCheck('x22', 'Apr-05-1963', x.dateCheck('4/5'), now, displayErrorsOnly)
+    now = testCheck('x23', 'Apr-11-1963', x.dateCheck('11'), now, displayErrorsOnly)
     x.expectedInputFormat = EF.Day_Month_Year
-    testCheck('x23', '03-Apr-1963', x.dateCheck('3'), displayErrorsOnly)
+    now = testCheck('x23', '03-Apr-1963', x.dateCheck('3'), now, displayErrorsOnly)
+    now = testCheck('x24', '02-Apr-1963', str(x.subtractDay()), now, displayErrorsOnly)
+    x.allowShortCutKeys = False
+    now = testCheck('x25', '01-Jan-1900', x.dateCheck('--'), now, displayErrorsOnly)
+    now = testCheck('x26', 'False', str(x.isValid), now, displayErrorsOnly)
+    now = testCheck('x27', '01-Sep-1963', str(x.dateCheck('01-09-63')), now, displayErrorsOnly)
+    now = testCheck('chk2', '23-Sep-1963', str(chk.dateCheck('23-09-63')), now, displayErrorsOnly)
+    now = testCheck('x28', '01-Sep-1963', x.formattedDate, now, displayErrorsOnly)
+
+    deltaTime = (now - startTime)
+
+    print('----------- Testing Ended ' + str(now.time()) + ' in ' + ("%.6f" % deltaTime.total_seconds()) + ' seconds -----------')
+
